b19579936abe075e5ba7c1b0f21d8fbd8e404dda
Eduardo Lopez <eduardo.bonfimlopez@hotmail.com>
2023-02-14 14:49:18 -0300

    Adicionar arquivos de projeto.

---
+++ LedscanQTD/Conexao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedscanQTD
+{
+    internal class Conexao
+    {
+        //encapsulamento para chegar no banco
+        private static string _ROTA = "Data Source = 192.168.108.16\\SQLEXPRESS; Initial Catalog = ROTA; Persist Security Info=True;User ID = eduardo_ucb; Password=3DSONICeu@";
+        public static string ROTA
+        {
+            get { return _ROTA; }
+            set { _ROTA = value; }
+
+        }
+    }
+}
---
+++ LedscanQTD/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LedscanQTD
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String UpdateQTD = "";
+
+        private void Update()
+        {
+            //Abre conexão com o  banco
+            SqlConnection conm = new SqlConnection(Conexao.ROTA);
+            SqlCommand comande1 = new SqlCommand(@"UPDATE [ROTA].dbo.[API_NF] SET [N_Qtd] = @QTD WHERE N_Nota_Fiscal=@NOTA AND [N_Modelo]=@MOD ", conm);
+
+            //cada um com sua variável
+            comande1.Parameters.Add("@MOD", SqlDbType.VarChar).Value = textBox2.Text;
+            comande1.Parameters.Add("@NOTA", SqlDbType.VarChar).Value = textBox1.Text;
+            comande1.Parameters.Add("@QTD", SqlDbType.Int).Value = Convert.ToInt32(UpdateQTD);
+
+            conm.Open();
+            comande1.ExecuteScalar();
+
+            //Fecha Conexão com o banco
+            conm.Close();
+            MessageBox.Show("Quantidade Atualizado com sucesso!");
+        }
+        // botão para atualização da quatidade 
+        private void tbupdate_Click(object sender, EventArgs e)
+        {
+            UpdateQTD = textBox3.Text.Trim();
+            Update();
+            textBox3.Clear();
+        }
+        
+        // textBox do modelo e N°NF
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
